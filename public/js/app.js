a4983f45f7a2cf716e808cf5c0633a8fff5cc8db
Felix Castro <felixcastro1993@gmail.com>
2020-08-05 23:09:35 -0500

    Modulo administrador y gestion de usuarios segunda version en laravel

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/jyc/Escritorio/Laravel Roles y permisos/Jhonatan-permisos/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/jyc/Escritorio/Laravel Roles y permisos/Jhonatan-permisos/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\sdaa\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\sdaa\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
